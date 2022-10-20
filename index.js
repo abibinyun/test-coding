84db001e275a64031c1ea13d9a59cbbf95928011
bilal <abii.manyun@gmail.com>
2022-10-20 21:35:04 +0700

    update

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-// (1)
+// SOAL (1)
 
-// function perkalian(value) {
-//   for (i = value - 1; i >= 1; i--) {
-//     value *= i;
-//   }
-//   console.log(value);
-// }
-// perkalian(8);
+function perkalian(value) {
+  for (i = value - 1; i >= 1; i--) {
+    value *= i;
+  }
+  console.log(value);
+}
+perkalian(8);
 
-// (2)
+// SOAL (2)
 
-// function katadibalik(value) {
-//   let hasil = "";
-//   for (var i = value.length - 1; i >= 0; i--) {
-//     hasil += value[i];
-//   }
-//   console.log(hasil);
-// }
-// katadibalik("kucing garong");
+function katadibalik(value) {
+  let hasil = "";
+  for (let i = value.length - 1; i >= 0; i--) {
+    hasil += value[i];
+  }
+  console.log(hasil);
+}
+katadibalik("kucing garong");
 
-//(3)
+// SOAL (3)
 
 // Buatlah fungsi untuk menampilkan jumlah digit angka tergantung dimana posisi atau tempat dari angka dalam sebuah string “9.86-A5.321”!
 // Contoh: printDigitValue(‘9.86-A5.321’);
@@ -33,62 +33,88 @@
 // 20
 // 1
 
-// (4)
+function printDigitValue(value) {
+  let value1 = value.length;
+  let result = "";
 
-// let a = 3;
-// let b = 7;
+  for (i = 0; i <= value1 - 1; i++) {
+    if (!Number(value[i])) {
+      continue;
+    }
+    result += value[i];
+  }
+  console.log(result);
 
-// a = a + b;
-// b = a - b;
-// a = a - b;
-// console.log(a);
-// console.log(b);
+  let value2 = result.length - 1;
+  let fix = "";
+  for (i = 0; i <= value2; i++) {
+    fix += result[i];
+    for (j = value2; j > i; j--) {
+      fix += "0";
+    }
+    fix += "\n";
+  }
+  console.log(fix);
+}
 
-// (5)
+printDigitValue("9.86-A5.321");
 
-// function penyebut(input) {
-//   let nilai = input;
-//   let arr = ["", "satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan", "sepuluh", "sebelas"];
-//   let temp = "";
-//   if (nilai <= 0 || nilai > 100) {
-//     console.log("masukan bilangan 1-100");
-//   } else if (nilai < 12) {
-//     temp = " " + arr[nilai];
-//   } else if (nilai < 20) {
-//     temp = penyebut(nilai - 10) + "belas";
-//   } else if (nilai < 100) {
-//     temp = penyebut(nilai / 10) + "puluh" + penyebut(nilai % 10);
-//   } else {
-//     console.log("seratus");
-//   }
-//   console.log(temp);
-// }
-// penyebut(13);
+// SOAL (4)
 
-// (6)
+let a = 3;
+let b = 7;
 
-// let array = [1, 4, 7, 9, 12];
-// let low = 2;
-// let high = 15;
+a = a + b;
+b = a - b;
+a = a - b;
+console.log(a);
+console.log(b);
 
-// let result = [];
-// let hasil = "";
+// SOAL (5)
 
-// array.forEach((item) => {
-//   if (item >= low && item <= high) {
-//     result = [...result, item];
-//     hasil = result.join(" ");
-//     return (total = result.length);
-//   }
-// });
+function penyebut(input) {
+  let nilai = input;
+  let arr = ["", " satu", " dua", " tiga", " empat", " lima", " enam", " tujuh", " delapan", " sembilan", "sepuluh", "sebelas"];
+  let temp = "";
+  if (nilai < 0 || nilai > 100) {
+    console.log("masukan bilangan 1-100");
+  } else if (nilai < 12) {
+    temp = " " + arr[nilai];
+  } else if (nilai < 20) {
+    temp = arr[nilai - 10] + " belas";
+  } else if (nilai < 100) {
+    temp = arr[Math.floor(nilai / 10)] + " puluh" + arr[nilai % 10];
+  } else {
+    console.log("seratus");
+  }
+  console.log(temp);
+}
+penyebut(23);
 
-// console.log(hasil);
+// SOAL (6)
 
-// (7)
+let array = [1, 4, 7, 9, 12];
+let low = 2;
+let high = 15;
 
-// console.log(total);
+let result = [];
+let hasil = "";
 
-// (8)
+array.forEach((item) => {
+  if (item >= low && item <= high) {
+    result = [...result, item];
+    hasil = result.join(" ");
+    return (total = result.length);
+  }
+});
+
+console.log(hasil);
+
+// SOAL (7)
+
+console.log(total);
+
+// SOAL (8)
 
 let angka = 15;
 
@@ -104,7 +130,7 @@ for (i = 1; i <= angka; i++) {
   }
 }
 
-// (9)
+// SOAL (9)
 
 let array = [4, 2, 6, 88, 3, 11];
 let low = array[0];
@@ -123,7 +149,7 @@ for (i = 0; i < count; i++) {
 
 console.log(`high : ${low} low : ${high}`);
 
-// (10)
+// SOAL (10)
 
 function kabisat(input) {
   if (input % 4 == 0) {
